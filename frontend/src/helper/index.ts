f9653e23d2f6a7cc9b10a247b80bb79a8d32166c
Do Quang Tan <johndylan1995@gmail.com>
2025-01-07 00:15:53 +0700

    Update bun server handle frontend caddy dashboard

--- frontend/src/helper/index.ts
+++ frontend/src/helper/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 async function GetConfig() {
     const result = await axios({
         method: 'get',
-        url: 'http://192.168.31.251:3001',
+        url: '/api',
     });
     return result.data;
 }
---
+++ server/index.ts
@@ -0,0 +1,25 @@
+import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+
+app.use("/*", cors());
+
+app.use("/*", serveStatic({ root: "../frontend/build" }));
+
+app.get("/api", async (c) => {
+	const response = await fetch("http://localhost:2019/config", {
+		method: "GET",
+		headers: { "Content-Type": "application/json" },
+	});
+
+	const data = await response.json();
+
+	return c.json(data);
+});
+
+export default {
+	port: 3001,
+	fetch: app.fetch,
+};
